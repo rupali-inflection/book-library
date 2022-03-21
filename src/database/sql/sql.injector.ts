7ab7fbf8b519952e2a42e19c70c39f7490255883
Rupali Dinde <rupalidinde@gmail.com>
2022-03-21 15:58:01 +0530

    resolved roleprivilage repo dependancy

--- src/database/sql/sql.injector.ts
+++ src/database/sql/sql.injector.ts
@@ -1,6 +1,7 @@
 
 import { DependencyContainer } from 'tsyringe';
 import { DatabaseConnector_Sequelize } from './sequelize/database.connector.sequelize';
+import { RolePrivilegeRepo } from './sequelize/repositories/role privilege.repo';
 import { UserRepo } from './sequelize/repositories/user.repo';
 import { UserRoleRepo } from './sequelize/repositories/user.role.repo';
 
@@ -9,5 +10,6 @@ export class SQLInjector {
         container.register('IDatabaseConnector', DatabaseConnector_Sequelize);
         container.register('IUserRepo', UserRepo);
         container.register('IRoleRepo', UserRoleRepo);
+        container.register('IRolePrivilegeRepo', RolePrivilegeRepo);
     }
 }
